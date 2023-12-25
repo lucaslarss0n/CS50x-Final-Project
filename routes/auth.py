1bb8a7cc5b57746771222bf290af2fbcc06bc26d
doktorn <65681213+doktoorn@users.noreply.github.com>
2023-12-25 21:47:39 +0100

    Temporary logo top right and some password validation

--- routes/auth.py
+++ routes/auth.py
@@ -19,6 +19,12 @@ def register():
 
         # Add your validation and registration logic here
 
+        # Example validation: Check if username already exists
+        if User.query.filter_by(username=form_data['username']).first():
+            flash('Username already exists. Please try again.', 'error')
+            return redirect(url_for('register'))
+        
+        # Example validation: Check if passwords match
         if password == password_confirm:
             # Assuming User is your user model and it has a register method
             User.register(form_data['username'], password)
@@ -26,6 +32,15 @@ def register():
             return redirect(url_for('login'))
         else:
             flash('Passwords do not match. Please try again.', 'error')
+
+        # Password validation
+        if len(password) < 8:
+            flash('Password must be at least 8 characters.', 'error')
+            return redirect(url_for('register'))
+        elif len(password) > 50:
+            flash('Password must be less than 50 characters.', 'error')
+            return redirect(url_for('register'))
+        
 
     return render_template('register.html', title='Register', form_data=form_data)
 

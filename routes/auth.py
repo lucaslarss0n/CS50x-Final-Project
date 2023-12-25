50c1e2652d37906a48bc0ea60972e10a69d36767
doktorn <65681213+doktoorn@users.noreply.github.com>
2023-12-25 01:17:10 +0100

    Implemented python and html for the register page

--- routes/auth.py
+++ routes/auth.py
@@ -3,11 +3,29 @@ from app import app
 # if i need to import models:
 # from app.models import User
 
-@app.route('/signin')
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
     return render_template('signin.html', title='Sign In')
 
-@app.route('/register')
+from flask import request, flash, redirect, url_for
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title='Register')
+    form_data = {}
+    if request.method == 'POST':
+        form_data['username'] = request.form.get('username')
+        password = request.form.get('password')
+        password_confirm = request.form.get('password_confirm')
+
+        # Add your validation and registration logic here
+
+        if password == password_confirm:
+            # Assuming User is your user model and it has a register method
+            User.register(form_data['username'], password)
+            flash('Registration successful. Please log in.', 'success')
+            return redirect(url_for('signin'))
+        else:
+            flash('Passwords do not match. Please try again.', 'error')
+
+    return render_template('register.html', title='Register', form_data=form_data)
 
